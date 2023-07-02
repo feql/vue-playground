e83728476d664cb86412e2eec629a61a59f03b85
Nyola Mike <nyolamike@Nyolas-MacBook-Pro.local>
2023-07-02 22:47:33 +0300

    the queries aspet apnel

--- src/components/QueriesAspectPanel.vue
+++ src/components/QueriesAspectPanel.vue
@@ -17,7 +17,7 @@
                 name="email"
                 id="email"
                 class="bg-zinc-100 block w-full  rounded-none rounded-l-sm border-0 py-0.5 pl-8 text-gray-900  placeholder:text-gray-300 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                style="margin-left: 2px;"
+                style="margin-left: 0px;"
                 placeholder="..."
               />
             </div>
@@ -64,8 +64,8 @@
             class="flex gap-x-4 py-2 p-2 cursor-pointer  ml-5 text-sm "
             :class="{
               'bg-slate-200': index != activeIndex,
-              'bg-slate-900': index == activeIndex,
-              'text-white font-bold': index == activeIndex,
+              'bg-[#f0f0f0]': index == activeIndex,
+              'text-slate-900 font-bold': index == activeIndex,
               'hover:bg-slate-200': index != activeIndex,
               'rounded-tl-md': index == 0, 
               'rounded-bl-md': index == (queries.length-1), 
@@ -77,21 +77,76 @@
           </li>
         </ul>
       </div>
-      <div class="w-8/12 h-full bg-slate-900"></div>
+      <div class="w-8/12 h-full bg-slate-900 flex flex-col text-white">
+        <div>
+          <v-ace-editor
+            v-model:value="queryCodeEditorContent"
+            @init="editorInit"
+            lang="javascript"
+            theme="monokai"
+            style="height: 300px" />
+        </div>
+        <div class="bg-[#f0f0f0] pl-10 flex flex-row ">
+          <div class="flex flex-row flex-grow items-center justify-between border-b border-b border-b-slate-400">
+            <div class="flex flex-row flex-grow items-center">
+              <button
+                type="button"
+                class="mr-2 my-2.5 ml-2 inline-flex items-center gap-x-1.5 rounded-sm px-2.5 py-1.5 text-xs font-semibold text-slate-300 shadow-md bg-zinc-800 hover:bg-zinc-900  hover:text-slate-200"
+              >
+                <PlayIcon size="20" class="-ml-0.5 h-4 w-4" aria-hidden="true" />
+                Execute
+              </button>
+              <span class="mr-2 bg-zinc-800 text-xs px-2.5 py-1.5 rounded-sm text-slate-300">
+                Method: <b class="text-slate-100">POST</b>
+              </span>
+              <span class="mr-2 bg-zinc-800 text-xs px-2.5 py-1.5 rounded-sm text-slate-300">
+                Status: <b class="text-slate-100">POST</b>
+              </span>
+            </div>
+            <div class="flex flex-row items-center">
+              <button
+                type="button"
+                class="mr-2 my-2.5 ml-2 inline-flex items-center gap-x-1.5 rounded-sm px-2.5 py-1.5 text-xs font-semibold text-slate-300 shadow-md bg-zinc-800 hover:bg-zinc-900  hover:text-slate-200"
+              >
+                <PencilIcon size="20" class="-ml-0.5 h-3 w-3" aria-hidden="true" />
+                Edit
+              </button>
+            </div>
+            
+          </div>
+          
+        </div>
+        <div>
+          <v-ace-editor
+            v-model:value="resultsCodeEditorContent"
+            @init="editorInit"
+            lang="javascript"
+            theme="monokai"
+            style="height: 300px" />
+        </div>
+        <div class="pl-10 flex flex-row items-center flex-grow">
+          <span class="text-xs">Last Executed On: <b>2023-06-07 12:34:23 PM</b></span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 
 <script>
-import { PlusIcon, TableCellsIcon, RectangleGroupIcon, BarsArrowUpIcon, MagnifyingGlassIcon } from '@heroicons/vue/24/solid'
+import { PlusIcon, TableCellsIcon, RectangleGroupIcon, BarsArrowUpIcon, MagnifyingGlassIcon, PlayIcon, PencilIcon } from '@heroicons/vue/24/solid'
+import { VAceEditor } from 'vue3-ace-editor';
+
 export default {
   components: {
     PlusIcon,
     TableCellsIcon,
     RectangleGroupIcon,
     BarsArrowUpIcon, 
-    MagnifyingGlassIcon
+    MagnifyingGlassIcon,
+    PlayIcon,
+    PencilIcon,
+    VAceEditor
   },
   data() {
     return {
@@ -127,7 +182,9 @@ export default {
           lastExecutedOn: ''
         }
       ],
-      activeIndex: -1
+      activeIndex: -1,
+      queryCodeEditorContent: "{\n/*Write your query here*/\n}",
+      resultsCodeEditorContent: "\n/*Query results are displayed here*/\n"
     }
   },
   methods: {
